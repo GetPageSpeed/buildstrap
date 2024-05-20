289862a264f94578081e64337893ca67d25d764b
Danila Vershinin <ciapnz@gmail.com>
2024-05-21 00:21:00 +0700

    Enable rolling release builds and remove forced git pull
    The buildscript configuration has been updated to enable rolling release builds. Furthermore, the forced git pull command was removed from the cron.sh script, thereby preventing discarding of any local changes during the automated git operations.

--- cron.sh
+++ cron.sh
@@ -19,8 +19,6 @@ git checkout main
 # navigate to ../rpmbuilder and commit any changes
 cd ../rpmbuilder || exit 1
 git pull --quiet
-# force git pull discarding any local changes
-git pull --force
 git add --all .
 git commit -m "Updated from buildstrap"
 git push --force --all origin
